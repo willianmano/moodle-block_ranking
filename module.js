dd292ae15988035e69fd91b2202b7864c3bdbb38
Willian Mano <willianmanoaraujo@gmail.com>
2017-04-18 20:34:30 -0300

    Removes module.js

--- module.js
+++
@@ -1,8 +0,0 @@
-M.block_ranking = {
-    inittabview: function(Y) {
-        Y.use("tabview", function(Y) {
-            var tabview = new Y.TabView({srcNode: '#ranking-tabs'});
-            tabview.render();
-        });
-    }
-};
